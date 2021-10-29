c9507017cc6cf5388c28f1d2cb0ab7f380545d34
mryota1274 <91864700+mryota1274@users.noreply.github.com>
2021-10-29 16:57:14 +0900

    Update Highest_and_Lowest.py

--- 7kyu/Highest_and_Lowest.py
+++ 7kyu/Highest_and_Lowest.py
@@ -2,7 +2,4 @@ def high_and_low(numbers):
     # ...
     strlist = numbers.split(' ')
     numlist = list(map(int,strlist))
-    maxnum = str(max(numlist))
-    minnum = str(min(numlist))
-    
-    return maxnum+" "+minnum
+    return str(max(numlist)) + " " + str(min(numlist))
