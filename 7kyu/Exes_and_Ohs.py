3ef39351cdc69fd1740eae73ef821ed1304ce5ac
mryota1274 <91864700+mryota1274@users.noreply.github.com>
2021-11-08 10:30:41 +0900

    Update Exes_and_Ohs.py

--- 7kyu/Exes_and_Ohs.py
+++ 7kyu/Exes_and_Ohs.py
@@ -1,8 +1,3 @@
 def xo(s):
     s=s.lower()
-    if s.count("x")==s.count("o"):
-        return True
-    elif s.count("x")==0 and s.count("o")==0:
-        return True
-    else:
-        return False
+    return s.count('x')==s.count('o')
